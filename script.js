951e8615c4a090075d48cd26443f75163246d69f
VendrickNZ <jakibisherwood88@gmail.com>
2022-11-17 11:13:58 +1300

    finished

--- script.js
+++ script.js
@@ -17,17 +17,36 @@ function shuffle() {
 let getComputerChoice = () => shuffle();
 
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection, computerSelection)
+    let winner = 'player';
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        draw()
+        draw();
+        winner = 'draw'
     } else {
     (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") ? playerRoundWinner(playerSelection, computerSelection) :
     (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") ? playerRoundWinner(playerSelection, computerSelection) :
     (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") ? playerRoundWinner(playerSelection, computerSelection) : 
-    computerRoundWinner(playerSelection, computerSelection);
+    computerRoundWinner(playerSelection, computerSelection, winner = 'computer');
     }
+    return winner;
 }
 
 let playerRoundWinner = (playerSelection, computerSelection) => console.log(`You win! ${playerSelection} beats ${computerSelection}`);
 let computerRoundWinner = (playerSelection, computerSelection) => console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-let draw = () => console.log('Draw!')
+let draw = () => console.log('Draw!');
+
+function game() {
+    let totalWins = 0, nPlayerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        let playerSelection = prompt("Please enter rock, paper, or scissors");
+        winner = playRound(playerSelection, computerSelection);
+        
+        if (winner != "draw") {
+            if (winner === "player") {
+                nPlayerWins++;
+            }
+            totalWins++;
+        }
+    }
+    (nPlayerWins > totalWins / 2) ? console.log("You win!") : nPlayerWins == totalWins ? (console.log("Draw!")) : console.log("You lost!");
+}
